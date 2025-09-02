1a82f217c7927837a5a4fe18afbd8c093a5cb77e
Scott <87299332+gw20eScott@users.noreply.github.com>
2025-09-02 13:02:05 +0200

    v1.2.1 - removing loading screen

--- app/loading.jsx
+++
@@ -1,7 +0,0 @@
-import LoadingSpinner from '@/core/components/LoadingSpinner'
-
-export default function Loading() {
-    return (
-        <LoadingSpinner message="Loading Application..." minHeight="100vh" size={80} color="primary" />
-    )
-}
